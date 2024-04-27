6f62ec44eac605d9752bde9e70db62eb09e6075c
root <root@vwakesahu>
2024-04-27 23:59:01 +0530

    URI CHNAGE

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,9 @@ const path = require("path");
 
 const errorMiddleware = require("./middleware/error")
 
-// if (process.env.NODE_ENV !== "PRODUCTION") {
-//     require("dotenv").config({path:"backend/config/config.env"});
-// }
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({path:"backend/config/config.env"});
+}
 
 app.use(express.json());
 app.use(cookieParser());
