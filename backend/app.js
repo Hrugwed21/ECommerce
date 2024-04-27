6261483e9084f667038cd5a948393d7605c2cc85
Vivek Sahu <viveksahu1762@gmail.com>
2024-04-28 01:37:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,20 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // You can also set other CORS headers as needed
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+  } else {
+      next();
+  }
+});
+
 //Route imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
