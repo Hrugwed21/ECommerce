2a9cbea96a411bfcdcf888e2372aa2fc5eaf48e0
Vivek Sahu <viveksahu1762@gmail.com>
2024-04-28 01:38:35 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,15 @@ import store from './store';
 
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import axios from 'axios';
 
 const options = {
   timeout: 5000,
   position: positions.BOTTOM_CENTER,
   transition: transitions.SCALE,
 };
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URI
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
