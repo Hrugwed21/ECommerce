af67e232d47ef9b7b9d581e7506e99659e3fe1df
root <root@vwakesahu>
2024-04-28 00:00:00 +0530

    URI CHNAGE

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,9 @@ process.on("uncaughtException",(err) => {
 });
 
 
-if (process.env.NODE_ENV !== "PRODUCTION") {
+// if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({path:"backend/config/config.env"});
-}
+// }
 
 
 //connecting to database
